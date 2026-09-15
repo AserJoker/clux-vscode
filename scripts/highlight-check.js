d4cd7ff5c28e5fdeee485157a304fcb0dc1030b7
aserjoker <aserjoker@outlook.com>
2026-09-15 11:59:37 +0800

    feat(syntax): 高亮 extends 关键字运算符（keyword.operator.clux）
    - cast-keyword 匹配 \b(as|extends)\b
    - 高亮自检样本加 'var ext = i32 extends i64;'

--- scripts/highlight-check.js
+++ scripts/highlight-check.js
@@ -35,6 +35,7 @@ const wasmBin = vsco.loadWASM;
     'var c = \'z\';',
     'var ok = true && !false;',
     'var u = undefined;',
+    'var ext = i32 extends i64;',
     'x += 1;',
     'while x > 0 { x--; }',
     'comptime func add(a: i32, b: i32): i32 { return a + b; }',
